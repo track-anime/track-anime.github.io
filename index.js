2e2ef17bcd24a7039706a57f7c7cb5fe5ec11e23
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-06-09 01:45:20 +0300

    //

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ function setVideoInfo(e) {
         // VideoPlayer.contentWindow.location.href = e.imdb ? `https://dygdyg.github.io/DygDygWEB/svetacdn.htm?menu_default=menu_button&imdb=${e.imdb}` : `https://dygdyg.github.io/DygDygWEB/svetacdn.htm?menu_default=menu_button&title=${e.material_data.anime_title}`
         // let url = `https://torlook.in/?search=${e.material_data.anime_title}`
         let url = `https://darklibria.it/search?find=${e.material_data.anime_title}`
-        window.open(url)
+        window.open(`https://darklibria.it/search?find=${e.material_data.anime_title}`)
         if (ev.shiftKey) {
         } else {
             // VideoPlayer.contentWindow.location.href = url
