c34c2f6be1e7d64e1cfeb716bed2b66abc08d4e9
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-06-22 06:22:38 +0300

    //

--- index.js
+++ index.js
@@ -287,7 +287,7 @@ function setVideoInfo(e) {
         </div>
     ` });
 
-    fetch(`//youtube.googleapis.com/youtube/v3/search?part=snippet&q=${e.russian}${encodeURIComponent(" трейлер на русском")}&key=AIzaSyAQS-Vh1GcuAYoKYy-1wOt0CSwTDEB39wQ`, {
+    fetch(`//youtube.googleapis.com/youtube/v3/search?part=snippet&q=${e.russian}${encodeURIComponent(" трейлер на русском anidub")}&key=AIzaSyAQS-Vh1GcuAYoKYy-1wOt0CSwTDEB39wQ`, {
         headers: {
             'Referer': location.href
         }
