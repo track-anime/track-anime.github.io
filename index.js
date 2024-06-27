46a20257c8a340f89472388400c93cfd13562350
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-06-27 04:10:02 +0300

    //

--- index.js
+++ index.js
@@ -1385,7 +1385,7 @@ function formatDate(isoDateString) {
 }
 function playSound(soundFile, vol) {
     var audioElement = new Audio(soundFile);
-    audioElement.volue = vol?vol:1;
+    audioElement.volue = vol?vol:1.0;
     audioElement.preload = 'auto';
     audioElement.play();
 }
