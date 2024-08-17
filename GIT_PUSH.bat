e95f817e37ba8c4030a22917687d6c489a7511ea
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-08-17 06:10:35 +0300

    17-08-2024_6-10-34-82

--- GIT_PUSH.bat
+++ GIT_PUSH.bat
@@ -7,7 +7,7 @@ scan.exe
 for /f "tokens=1-4 delims=. " %%a in ('echo %date%') do set dt=%%a-%%b-%%c
 for /f "tokens=1-4 delims=:., " %%a in ('echo %time%') do set tm=%%a-%%b-%%c-%%d
 set datetime=%dt%_%tm%
-echo %datetime%
+echo %datetime% >vers.info
 git add .
 git commit -m "%datetime%"
 git push origin main
