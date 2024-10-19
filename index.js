2790d7e35c19c59800d53cce2e9d6f4c7e9d6184
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-10-20 00:29:42 +0300

    20-10-2024_0-29-42

--- index.js
+++ index.js
@@ -78,12 +78,7 @@ localStorage.setItem('BaseAnime', JSON.stringify(base_anime));
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // console.log("translation", base_anime?.translation[0] == "string", base_anime?.translationActive[0] == "string")
 
-if (!base_anime?.translation || typeof base_anime.translation[0] !== "string") {
-    base_anime.translation = [];
-}
-if (!base_anime?.translationActive || typeof base_anime.translationActive[0] !== "string") {
-    base_anime.translationActive = [];
-}
+
 
 
 ///////////////////////////////////////////// Меняет иконку на локальном сайте ////////////////////////////
@@ -1291,6 +1286,12 @@ function ClearFavorite() {
 
 
 function VoiceSettingsMenu() {
+    if (!base_anime?.translation || typeof base_anime.translation[0] !== "string") {
+        base_anime.translation = [];
+    }
+    if (!base_anime?.translationActive || typeof base_anime.translationActive[0] !== "string") {
+        base_anime.translationActive = [];
+    }
     VoiceSettings.innerHTML = ""
     const checkboxList = document.getElementById('checkbox-list');
     const buttonContainer = document.getElementById('button-container');
