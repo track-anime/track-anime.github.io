7349778c7838c8a9089d4e5ca627d5dca1c77953
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-10-26 06:54:02 +0300

    2024-10-26 03:54:02

--- index.js
+++ index.js
@@ -460,7 +460,7 @@ function setVideoInfo(e) {
     VideoInfo.e = e
     const tv = e.kind ? ` [${e.kind.toUpperCase()}]` : ""
     VideoInfo.info.cover.src = `https://shikimori.one${e.image.original}`;
-    if(VideoInfo.info.cover.src.includes("missing_original.jpg")) VideoInfo.info.cover.src = "pedro.gif"
+    if(VideoInfo.info.cover.src.includes("missing_original.jpg")) VideoInfo.info.cover.src = "404_static.png"
     VideoInfo.info.title.childNodes[0].nodeValue = e.russian ? `${tv}` : "?";
     VideoInfo.info.title.querySelector("a").textContent = e.russian ? `${e.russian}` : "?";
     VideoInfo.info.title.querySelector("a").href = e.russian ? `${window.location.origin + window.location.pathname}?seartch=${e.russian ? encodeURIComponent(e.russian) : "404.html"}` : "404.html";
@@ -1539,7 +1539,7 @@ function add_cart(e) {
     target.classList.add('cart-target');
     cart.appendChild(target);
 
-    if(e.cover.includes("missing_original.jpg")) e.cover = "pedro.gif"
+    if(e.cover.includes("missing_original.jpg")) e.cover = "404_static.png"
 
     const imgTop = document.createElement('div');
     imgTop.style.backgroundImage = `url(${e.cover}`;
