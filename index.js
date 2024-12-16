d8a2c11e8bb8ee61ae2a69aca73c2618aec08d45
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-12-16 07:03:55 +0300

    2024-12-16 04:03:55

--- index.js
+++ index.js
@@ -513,7 +513,7 @@ function setVideoInfo(e) {
     VideoInfo.e = e
     const tv = e.kind ? ` [${e.kind.toUpperCase()}]` : ""
     VideoInfo.info.cover.src = `https://shikimori.one${e.image.original}`;
-    if (VideoInfo.info.cover.src.includes("missing_original.jpg")) VideoInfo.info.cover.src = getCover(e.id)
+    if (VideoInfo.info.cover.src.includes("missing_original.jpg")) VideoInfo.info.cover.src = `http://107.173.19.4/cover.php?id=${e.id}`
     VideoInfo.info.title.childNodes[0].nodeValue = e.russian ? `${tv}` : "?";
     VideoInfo.info.title.querySelector("a").textContent = e.russian ? `${e.russian}` : "?";
     VideoInfo.info.title.querySelector("a").href = e.russian ? `${window.location.origin + window.location.pathname}?seartch=${e.russian ? encodeURIComponent(e.russian) : "404.html"}` : "404.html";
@@ -1638,7 +1638,7 @@ function add_cart(e) {
     const target = document.createElement('div');
     target.classList.add('cart-target');
     cart.appendChild(target);
-    if (e.cover.includes("missing_original.jpg")) e.cover = getCover(e.shikimori)
+    if (e.cover.includes("missing_original.jpg")) e.cover = `http://107.173.19.4/cover.php?id=${e.shikimori}`
 
     const imgTop = document.createElement('div');
     imgTop.style.backgroundImage = `url(${e.cover}`;
