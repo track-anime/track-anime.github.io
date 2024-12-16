28499c0c336261414d7befb09fb41f84b3e15ceb
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-12-16 07:04:38 +0300

    123

--- index.js
+++ index.js
@@ -1642,6 +1642,7 @@ function add_cart(e) {
 
     const imgTop = document.createElement('div');
     imgTop.style.backgroundImage = `url(${e.cover}`;
+    
 
     imgTop.src = e.cover;
     imgTop.classList.add('cart-img-top');
