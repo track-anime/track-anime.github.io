6dcf798c8fd53165d618f96252a9728052c66f25
DygDyg <dygdyg@inbox.ru>
2025-07-08 02:43:33 +0300

    2025-07-07 23:43:33

--- index.js
+++ index.js
@@ -2557,7 +2557,7 @@ function GetKodiScan(data, revers) {
         }
         //(e.type == 'anime-serial' || e.type == "anime") &&
         if (e.translation.type == "voice" && e.shikimori_id) {  //&& e.material_data.countries != "Китай" //&& e.material_data.shikimori_rating > 0
-            if (_CheckRepeats(e.shikimori_id) && (BaseAnimeCurrent[e.shikimori_id]?.episode <= e.last_episode)) {
+            if (_CheckRepeats(e.shikimori_id) ){//&& (BaseAnimeCurrent[e.shikimori_id]?.episode <= e.last_episode)) {
                 debug.log("Hides Repeats")
                 return
             }
