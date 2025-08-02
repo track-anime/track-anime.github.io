4450b85d985f4c906accf067756d758a3f58efe1
DygDyg <dygdyg@inbox.ru>
2025-08-02 11:57:20 +0300

    2025-08-02 08:57:19

--- index.php
+++ index.php
@@ -18,12 +18,17 @@ if (isset($_GET['shikimori_id']) && !empty($_GET['shikimori_id'])) {
         $data = json_decode($response, true);
         // Проверяем, есть ли поле russian в ответе
         if (isset($data['russian']) && !empty($data['russian'])) {
+            $content = preg_replace('/<title>[^<]*<\/title>/i', '<title>TA: ['.$data['kind']."] ".$data['russian'].'</title>', $content);
 
-            // Заменяем существующий метатег description на новый
-            $content = preg_replace('/<title>[^<]*<\/title>/i', '<title>TA: ['.$data['kind']."] ".$data['russian'].'"</title>', $content);
+
+            $content = preg_replace('/<meta\s+name="title"\s+content="[^"]*"\s*\/?>/i', '<meta og:title" content="TA: ['.$data['kind']."] ".$data['russian'].'">', $content);
+            $content = preg_replace('/<meta\s+property="og:title"\s+content="[^"]*"\s*\/?>/i', '<meta og:title" content="TA: ['.$data['kind']."] ".$data['russian'].'">', $content);
+            $content = preg_replace('/<meta\s+property="twitter:title"\s+content="[^"]*"\s*\/?>/i', '<meta twitter:title" content="TA: ['.$data['kind']."] ".$data['russian'].'">', $content);
+            
             $content = preg_replace('/<meta\s+name="description"\s+content="[^"]*"\s*\/?>/i', '<meta name="description" content="['.$data['kind']."] ".$data['russian'].'">', $content);
             $content = preg_replace('/<meta\s+property="og:description"\s+content="[^"]*"\s*\/?>/i', '<meta property="og:description" content="['.$data['kind']."] ".$data['russian'].'">', $content);
             $content = preg_replace('/<meta\s+property="twitter:description"\s+content="[^"]*"\s*\/?>/i', '<meta property="twitter:description" content="['.$data['kind']."] ".$data['russian'].'">', $content);
+
             $content = preg_replace('/<meta\s+property="og:image"\s+content="[^"]*"\s*\/?>/i', '<meta property="og:image" content="https://server.dygdyg.ru/cover2.php?id='.$shikimori_id.'">', $content);
             $content = preg_replace('/<meta\s+property="twitter:image"\s+content="[^"]*"\s*\/?>/i', '<meta property="twitter:image" content="https://server.dygdyg.ru/cover2.php?id='.$shikimori_id.'">', $content);
         }
