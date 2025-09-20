3d88ceba804a6c6986a2f52a0cbb67103027425f
DygDyg <dygdyg@inbox.ru>
2025-09-21 02:27:30 +0300

    2025-09-20 23:27:29

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 // Читаем содержимое файла index.htm
 $content = file_get_contents('index.htm');
 $debug_script = "<div></div>";
+
 // Проверяем наличие GET параметра shikimori_id
 if (isset($_GET['shikimori_id']) && !empty($_GET['shikimori_id'])) {
     // Получаем shikimori_id из GET параметра
@@ -16,26 +17,40 @@ if (isset($_GET['shikimori_id']) && !empty($_GET['shikimori_id'])) {
     if ($response !== false) {
         // Декодируем JSON ответ
         $data = json_decode($response, true);
-        // Проверяем, есть ли поле russian в ответе
+        
         if (isset($data['russian']) && !empty($data['russian'])) {
-            $content = preg_replace('/<title>[^<]*<\/title>/i', '<title>TA: ['.$data['kind']."] ".$data['russian'].'</title>', $content);
+            $title = "TA: [{$data['kind']}] {$data['russian']}";
+            $desc  = "[{$data['kind']}] {$data['russian']}";
+            $img   = "https://server.dygdyg.ru/cover2.php?id={$shikimori_id}";
 
+            // Title
+            $content = preg_replace('/<title>[^<]*<\/title>/i', "<title>{$title}</title>", $content);
 
-            $content = preg_replace('/<meta\s+name="title"\s+content="[^"]*"\s*\/?>/i', '<meta og:title" content="TA: ['.$data['kind']."] ".$data['russian'].'">', $content);
-            $content = preg_replace('/<meta\s+property="og:title"\s+content="[^"]*"\s*\/?>/i', '<meta og:title" content="TA: ['.$data['kind']."] ".$data['russian'].'">', $content);
-            $content = preg_replace('/<meta\s+property="twitter:title"\s+content="[^"]*"\s*\/?>/i', '<meta twitter:title" content="TA: ['.$data['kind']."] ".$data['russian'].'">', $content);
-            
-            $content = preg_replace('/<meta\s+name="description"\s+content="[^"]*"\s*\/?>/i', '<meta name="description" content="['.$data['kind']."] ".$data['russian'].'">', $content);
-            $content = preg_replace('/<meta\s+property="og:description"\s+content="[^"]*"\s*\/?>/i', '<meta property="og:description" content="['.$data['kind']."] ".$data['russian'].'">', $content);
-            $content = preg_replace('/<meta\s+property="twitter:description"\s+content="[^"]*"\s*\/?>/i', '<meta property="twitter:description" content="['.$data['kind']."] ".$data['russian'].'">', $content);
+            // Title tags
+            $content = preg_replace('/<meta\s+name="title"\s+content="[^"]*"\s*\/?>/i', 
+                "<meta name=\"title\" content=\"{$title}\">", $content);
+            $content = preg_replace('/<meta\s+property="og:title"\s+content="[^"]*"\s*\/?>/i', 
+                "<meta property=\"og:title\" content=\"{$title}\">", $content);
+            $content = preg_replace('/<meta\s+property="twitter:title"\s+content="[^"]*"\s*\/?>/i', 
+                "<meta property=\"twitter:title\" content=\"{$title}\">", $content);
 
-            $content = preg_replace('/<meta\s+property="og:image"\s+content="[^"]*"\s*\/?>/i', '<meta property="og:image" content="https://server.dygdyg.ru/cover2.php?id='.$shikimori_id.'">', $content);
-            $content = preg_replace('/<meta\s+property="twitter:image"\s+content="[^"]*"\s*\/?>/i', '<meta property="twitter:image" content="https://server.dygdyg.ru/cover2.php?id='.$shikimori_id.'">', $content);
+            // Description
+            $content = preg_replace('/<meta\s+name="description"\s+content="[^"]*"\s*\/?>/i', 
+                "<meta name=\"description\" content=\"{$desc}\">", $content);
+            $content = preg_replace('/<meta\s+property="og:description"\s+content="[^"]*"\s*\/?>/i', 
+                "<meta property=\"og:description\" content=\"{$desc}\">", $content);
+            $content = preg_replace('/<meta\s+property="twitter:description"\s+content="[^"]*"\s*\/?>/i', 
+                "<meta property=\"twitter:description\" content=\"{$desc}\">", $content);
+
+            // Images
+            $content = preg_replace('/<meta\s+property="og:image"\s+content="[^"]*"\s*\/?>/i', 
+                "<meta property=\"og:image\" content=\"{$img}\">", $content);
+            $content = preg_replace('/<meta\s+property="twitter:image"\s+content="[^"]*"\s*\/?>/i', 
+                "<meta property=\"twitter:image\" content=\"{$img}\">", $content);
         }
     }
     
-    
-    // Вставляем отладочный скрипт перед закрывающим тегом </body>
+    // Вставляем отладочный скрипт перед </body>
     $content = str_replace('</body>', $debug_script . '</body>', $content);
 }
 
